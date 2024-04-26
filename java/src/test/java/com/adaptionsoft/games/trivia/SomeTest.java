7aa19e237b73a001c8caeae3ff0b25318a29bb70
Vijay Patel <spiritedtechie@gmail.com>
2024-04-26 08:01:25 +0100

    Modernises the pom somewhat - java 11, junit 5 etc.

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,8 +1,10 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+
 
 public class SomeTest {
 
