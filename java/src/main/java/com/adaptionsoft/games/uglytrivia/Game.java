a760ad7640e3df30396cec723aed1f8dd498a4ac
Vijay Patel <spiritedtechie@gmail.com>
2024-05-01 12:29:00 +0100

    Switches to enummap for questions, as more performant.

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -1,10 +1,9 @@
 package com.adaptionsoft.games.uglytrivia;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 enum Category {
 	Pop, Science, Sports, Rock
@@ -72,7 +71,7 @@ public class Game {
 
 	private List<Player> players = new ArrayList<>();
 
-	private Map<Category, LinkedList<String>> questions = new HashMap<>();
+	private EnumMap<Category, LinkedList<String>> questions = new EnumMap<>(Category.class);
 
 	private int currentPlayerIndex = 0;
 
